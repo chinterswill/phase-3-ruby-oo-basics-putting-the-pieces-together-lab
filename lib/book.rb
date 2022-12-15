0685e82119eb136392b5eb8de9c80e1df9a9a132
Kamran Hussein <chinterswill@gmail.com>
2022-12-15 11:58:11 -0500

    commit tree 97f215d6c95a18a370479fd857b6aa2348f0140b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book 
+    attr_accessor :title, :author, :page_count, :genre
+    attr_reader :title
 
+
+    def initialize(title)
+        @title
+    end
+
+    def turn_page
+        puts 'Flipping the page...wow, you read fast!'
+    end
+end 
+
+book.title = 'And Then There Were None'
 
